364063bb96ebd219077aa0099b10771927aafd62
gangireddy009 <sudhagangireddy009@gmail.com>
2021-04-24 22:01:52 +0530

    first commit

---
+++ src/first.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+interface IState{
+
+}
+interface IProps{
+
+}
+class First extends React.Component<IState,IProps>{
+  render(){
+      return(
+          <h1>welcome to the reactjs.</h1>
+      )
+  }  
+}
+export default First;
